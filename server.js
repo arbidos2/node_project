a8ab3d53af8eeeeed698cf835331d4b95add7647
Young gyu Gweon <arbidos2@gmail.com>
2017-05-01 21:12:10 +0900

    node 1st

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+
+// app.get('/contactlist', function (req, res) {
+// 	console.log("I received a GET request")
+
+
+    // res.json(contactlist);
+// });
+
+app.listen(3000);
+console.log("Server running on port 3000. Hooray!");
