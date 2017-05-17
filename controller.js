d1c6b8ca068e7221620b95de83c496436adaee4a
Young gyu Gweon <arbidos2@gmail.com>
2017-05-17 18:13:10 +0900

    get_request_2

--- controller.js
+++ controller.js
@@ -2,40 +2,19 @@ var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope, $http) {
 
-    $http.get('/contactlist');
 
 	console.log("Hello world from controller");
 
-    person1 = {
-    	name: "Tim",
-    	email: "tim@email.com",
-    	number: "(111) 111 - 1111"
-    };
-
-    person2 = {
-    	name: "Emily",
-    	email: "emily@email.com",
-    	number: "(222) 222 - 2222"
-    };
-
-    person3 = {
-    	name: "John",
-    	email: "john@email.com",
-    	number: "(333) 333 - 3333"
-    };
-
-    var contactlist = [person1, person2, person3];
-	$scope.contactlist = contactlist;
-
-	// $http.get('/contactlist').success(function(response) {
-	// 	console.log("I got the data I requested");
+	// $scope.contactlist = contactlist;
 
 
-	// $http.get("welcome.htm")
-	// 	.then(function(response) {
-	// 		$scope.myWelcome = response.data;
-	// 	});
+    $http.get('/contactlist')
+    .then(function(response) {
+        console.log("I got the data I requested");
+        $scope.contactlist = response.data;
+    });
 
+	// .then(function(response) {
+		// $scope.myWelcome = response.data;
 	// });
-
 });
--- server.js
+++ server.js
@@ -6,7 +6,26 @@ app.use(express.static(__dirname));
 app.get('/contactlist', function (req, res) {
 	console.log("I received a GET request")
 
-    // res.json(contactlist);
+	person1 = {
+    	name: "Tim",
+    	email: "tim@email.com",
+    	number: "(111) 111 - 1111"
+    };
+
+    person2 = {
+    	name: "Emily",
+    	email: "emily@email.com",
+    	number: "(222) 222 - 2222"
+    };
+
+    person3 = {
+    	name: "John",
+    	email: "john@email.com",
+    	number: "(333) 333 - 3333"
+    };
+
+    var contactlist = [person1, person2, person3];
+    res.json(contactlist);
 });
 
 app.listen(3000);
